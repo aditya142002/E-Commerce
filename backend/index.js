9fd50856ad45f67d7fde8e4caa428e023e10512d
aditya singh <adisingh142002@gmail.com>
2025-08-24 00:50:29 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 // Database Connection with MongoDB
 mongoose.connect(process.env.MONGO_URL)
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 
 //API creatiion
 app.get("/",(req,res)=>{
@@ -48,7 +48,7 @@ app.use('/images',express.static('upload/images'))
 app.post("/upload",upload.single('product'),(req,res)=>{
     res.json({
         success:1,
-        image_url:`http://localhost:${port}/images/${req.file.filename}`
+        image_url:`https://e-commerce-1-qv70.onrender.com/images/${req.file.filename}`
     })
 })
 
