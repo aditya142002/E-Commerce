99a621db846634d2b863d409126e325323796c3e
aditya singh <adisingh142002@gmail.com>
2025-08-24 00:18:38 +0530

    bhai plz

--- Frontend/Ecommerce/src/Components/NewCollections/NewCollections.jsx
+++ Frontend/Ecommerce/src/Components/NewCollections/NewCollections.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './NewCollections.css'
 import new_collections from '../Assets/new_collections'
-import Item from '../Item/item'
+import Item from '../Item/Item'
 import { useState } from 'react'
 import { useEffect } from 'react'
 
